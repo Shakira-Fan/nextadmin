f51833dcf27c78fecca7f6bd011f18677ac05361
范綉莉 <shakira.fang@secom.com.tw>
2023-11-23 15:56:04 +0800

    add server action

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -14,7 +14,7 @@ const UserPage = async ({searchParams}) => {
     <div className={styles.container}>
       <div className={styles.top}>
         <Search placeholder="Search for a user..." />
-        <Link href="/dashboard/users/">
+        <Link href="/dashboard/users/add">
           <button className={styles.addButton}>Add New</button>
         </Link>
       </div>
---
+++ app/lib/actions.js
@@ -0,0 +1,30 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { User } from "./models";
+import { connectToDB } from "./utils";
+
+export const addUser = async (formData) => {
+
+  const { username, email, password, phone, address, isAdmin, isActive } =
+    Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    const newUser = new User({
+      username,
+      email,
+      password,
+      phone,
+      address,
+      isAdmin,
+      isActive,
+    });
+    await newUser.save();
+  } catch (error) {
+    console.log(error);
+    throw new Error("Failed to create user");
+  }
+
+  revalidatePath("/dashboard/users")
+};
